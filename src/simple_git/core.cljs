0523ed98503224b043e9c043ca71125683a4aed8
Maurício Szabo <mauricio.szabo@gmail.com>
2021-08-19 19:25:45 -0300

    Added "quick commit"

--- src/simple_git/core.cljs
+++ src/simple_git/core.cljs
@@ -1,10 +1,69 @@
 (ns simple-git.core
   (:require [simple-git.cmds :as cmds]
-            ["atom" :refer [CompositeDisposable]]))
+            [promesa.core :as p]
+            ["diff2html" :as diff]
+            ["atom" :refer [CompositeDisposable TextEditor]]))
 
 (def subscriptions (atom (CompositeDisposable.)))
 
 (defonce atom-state (atom nil))
+
+(defn generate-view [placeholder p]
+  (let [editor (TextEditor. #js {:mini true :placeholderText placeholder})
+        editor-view (.. js/atom -views (getView editor))
+        div (js/document.createElement "div")
+        panel (.. js/atom -workspace (addModalPanel #js {:item div}))
+        destroy-and-focus (fn []
+                            (.destroy panel)
+                            (.. js/atom -views (getView (.-workspace js/atom)) focus))]
+
+    (.. js/atom -views (getView panel) -style (setProperty "height" "100%"))
+    (.. div -style (setProperty "height" "100%"))
+    (.append div editor-view)
+    (doto (.-commands js/atom)
+          (.add (.-element editor) "core:confirm" (fn []
+                                                    (p/resolve! p (.getText editor))
+                                                    (destroy-and-focus)))
+          (.add (.-element editor) "core:cancel" (fn []
+                                                   (p/resolve! p nil)
+                                                   (destroy-and-focus))))
+    (p/do!
+     (p/delay 10)
+     (.focus editor-view))
+    div))
+
+(defn prompt! [placeholder]
+  (let [p (p/deferred)]
+    (generate-view placeholder p)
+    p))
+
+(defn- diff->html [diff-str]
+  (let [parsed (.parse diff diff-str)]
+    (.html diff parsed #js {:drawFileList true})))
+
+(defn diff-prompt! [placeholder diff-str]
+  (let [p (p/deferred)
+        html (generate-view placeholder p)
+        diff-elem (js/document.createElement "div")
+        style (.-style diff-elem)]
+    (.setProperty style "height" "100%")
+    (.setProperty style "overflow" "scroll")
+    (aset diff-elem "innerHTML" (diff->html diff-str))
+    (.. diff-elem -classList (add "native-key-bindings"))
+    (.append html diff-elem)
+    p))
+
+(defn- simplify [string]
+  (let [size (count string)]
+    (if (> size 30)
+      (str "..." (subs string (- size 30)))
+      string)))
+
+(defn- quick-commit! []
+  (p/let [file (cmds/current-file!)
+          {:keys [output]} (cmds/run-git "diff" "HEAD" file)
+          commit-msg (diff-prompt! (str "Commit message for " (simplify file)) output)]
+    (cmds/run-git-treating-errors "commit" file "-m" commit-msg)))
 
 (defn activate [state]
   (reset! atom-state state)
@@ -12,7 +71,12 @@
   (.add @subscriptions (.. js/atom -commands
                            (add "atom-text-editor"
                                 "git:add-current-file"
-                                #(cmds/run-git-treating-errors "add" (cmds/current-file!))))))
+                                #(cmds/run-git-treating-errors "add" (cmds/current-file!)))))
+  (.add @subscriptions (.. js/atom -commands
+                           (add "atom-text-editor"
+                                "git:quick-commit-current-file"
+                                quick-commit!))))
+
 
 (defn deactivate [state]
   (.dispose ^js @subscriptions))
